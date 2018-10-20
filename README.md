49737a08af18e6070008f32fe22e67f62a306a47
freewind <nowindlee@gmail.com>
2018-10-20 21:15:55 +0800

    demo

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-Vue Typescript Demo
-===================
+Vue Typescript Method Styles Demo
+=================================
 
+`vue-property-decorator`对typescript的方法声明支持不全面。
+
+下面两种写法，只有第1种正常支持。
 
 ```
-npm install
-npm run serve
+showMessage1() {
+  alert(this.message)
+}
 ```
+
+```
+showMessage2 = () => {
+  alert(this.message)
+}
+```
+
+第二种不能正确的拿到修改后的`message`的值。
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,28 @@
 <template>
-    <div class="hello">
-        {{ msg }}
-    </div>
+  <div class="hello">
+    <input type="text" v-model="message"/>
+    <button type="button" @click="showMessage1">Show Correct</button>
+    <button type="button" @click="showMessage2">Show Invalid</button>
+  </div>
 </template>
 
 <script lang="ts">
-    import {Component, Prop, Vue} from 'vue-property-decorator';
+  import {Component, Vue} from 'vue-property-decorator';
 
-    @Component
-    export default class HelloWorld extends Vue {
-        @Prop() private msg: string = "Hello Vue!";
+  @Component
+  export default class HelloWorld extends Vue {
+    message = "change me";
+    showMessage1() {
+      alert(this.message)
     }
+    showMessage2 = () => {
+      alert(this.message)
+    }
+  }
 </script>
 
 <style scoped>
-    .hello {
-        color: red;
-    }
+  .hello {
+    color: red;
+  }
 </style>
